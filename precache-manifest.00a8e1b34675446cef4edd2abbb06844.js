9d2ae6e7a18c987bb06d651776d9090c7a65a9f8
Simon <semen.zelenskiy@gmail.com>
2020-01-30 09:51:20 +0200

    Updates

---
+++ precache-manifest.00a8e1b34675446cef4edd2abbb06844.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4977aa6ea30752645b7bb96467ce189f",
+    "url": "/4service_TT/index.html"
+  },
+  {
+    "revision": "124067c9c42043d155cc",
+    "url": "/4service_TT/static/css/main.bd70e8b9.chunk.css"
+  },
+  {
+    "revision": "c157e8e4af43bc08d7e4",
+    "url": "/4service_TT/static/js/2.66fd64ef.chunk.js"
+  },
+  {
+    "revision": "da8e2c206479c7710b1f591b087d5954",
+    "url": "/4service_TT/static/js/2.66fd64ef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "124067c9c42043d155cc",
+    "url": "/4service_TT/static/js/main.ca176ca1.chunk.js"
+  },
+  {
+    "revision": "f1c1237cd831cbb067cd",
+    "url": "/4service_TT/static/js/runtime-main.6dd061b4.js"
+  }
+]);
